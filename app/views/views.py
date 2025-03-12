ef0cca2f2b2f6f10df7e1ba5f623d517b39e16f6
hyewonk815 <0815kimhw@naver.com>
2025-03-12 22:13:16 +0900

    views 수정

--- app/views/views.py
+++ app/views/views.py
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-# def home(request):
-#     return render(request, 'home.html')
-def home_search(request):
-    return render(request, 'home_search.html')
-=======
-
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-    
->>>>>>> 77caf802292f1db126b8bb623c010eb3e5833a9f
