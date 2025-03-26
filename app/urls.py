dc025ddcf93d2f06baa837c5a78001e9323966ef
hyewonk815 <0815kimhw@naver.com>
2025-03-26 20:08:06 +0900

    0326

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from app.views import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),  path('login/', views.login, name='login'), path('search/', views.search, name='search'),
 ]
