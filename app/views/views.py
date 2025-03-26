b2466dbece91b05ad40658fc2fd53848d7d66461
hyewonk815 <0815kimhw@naver.com>
2025-03-26 20:08:06 +0900

    0326

--- app/views/views.py
+++ app/views/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def login(request):
+    return render(request, 'login.html')
+def search(request):
+    return render(request, 'search.html')
 
